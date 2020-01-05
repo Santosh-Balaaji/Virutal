31b5b089dac292ef861c9bd6696955b738912725
Santosh Balaaji <sv.santosh2695@gmail.com>
2020-01-05 22:00:29 +1300

    Add project files.

---
+++ Virutal/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Virutal
+{
+
+    public class BaseClass
+    {
+        public  virtual void get()
+        {
+            Console.WriteLine("Inside Base class");
+        }
+    }
+
+    public class SubClass1 : BaseClass
+    {
+        public  override void get()
+        {
+            Console.WriteLine("Inside SubClass1");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            SubClass1 obj = new SubClass1();
+            obj.get();
+
+            BaseClass obj1 = new BaseClass();
+            obj1.get();
+        }
+    }
+}
